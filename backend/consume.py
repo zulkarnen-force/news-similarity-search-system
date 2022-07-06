439b3b38dece8ce5631f6e46a0bf32e0cbc4689d
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-07 06:13:42 +0700

    delete dead code and delete file development nodemon

--- backend/consume.py
+++ backend/consume.py
@@ -15,18 +15,11 @@ redis = redis.Redis(host=env.HOST, port=env.PORT, decode_responses=True)
     
 def insert_to_redis(filename, data):
     
-    try:
-        if (redis.exists(filename) == 0) :
-            if redis.rpush(filename, json.dumps(data)) != 0 :
-                console.success(f'file saved successfully on Redis with filename {filename}', severe=True, showTime=False)
-            else:
-                raise Exception('Failed save to redis')
-        else:
-            raise Exception(f'key with {filename} already exists on redis')
-    except DataError as e :
-        console.error(e.args, severe=True)
-    except Exception as e :
-        console.error(e, severe=True)
+    if (redis.rpush(filename, data) != 0) :
+        console.success(f'file saved successfully on Redis with filename {filename}', severe=True, showTime=False)
+    else:
+        raise Exception('Failed save to redis')
+    
         
              
         
@@ -48,33 +41,25 @@ def load_file_from_db(source:str, filename:str) :
     except Exception as e :
         console.error(e)
         
-        
-URL = 'http://localhost:8000/'
 
 def on_message(message):
     
     
     body:dict = json.loads(message.body)
     
-    # headers:dict = message.properties['headers']
-    # content_type:str = message.content_type
-    # filename:str = headers['filename']
-    # path:str = headers['path']
-    # console.error(path)
-    
     console.info(body, severe=True, showTime=False)
     filename:str = body['filename']
     path:str = body['path']
     
     try :
-        dataframe = load_file_from_db(URL+path, filename)
+        dataframe = load_file_from_db(BASE_URL+path, filename)
     
         json_data = dataframe.to_json(orient='records')
         object_data = json.loads(json_data)
         
         try :
             
-            insert_to_redis(filename, object_data)  
+            insert_to_redis(filename, json_data)  
             message.ack()
 
         except Exception as e:
@@ -83,56 +68,4 @@ def on_message(message):
         
     except Exception as e:
         console.error(f'{e} ~consume.py-2', severe=True, showTime=False)
-        message.ack()       
-        
-        
-    
-    
-    
-    # if dataframe is False:
-    #     file = open('backend/data.json', 'r') # testing
-    #     insert_to_redis(filename, json.load(file))
-    #     message.ack()
-        
-    # else:
-    #     json_data = dataframe.to_json(orient='records')
-    #     object_data = json.loads(json_data)
-        
-    #     insert_to_redis(filename, object_data)
-    #     message.ack()
-        
-
-    
-    
-
-
-# def on_message(message):
-#     loadfile = json.loads(message.body)
-#     link = env.LINK
-#     path = "/excel-data/"
-#     file = str(loadfile["filename"])
-#     print(loadfile)
-#     url = link+path+file
-#     print(url)
-    
-#     def load_data():
-#         if file.lower().endswith(('.xlsx','.xlx','.xls')) :
-#             data = pd.read_excel(url, engine='openpyxl')
-#             return data
-#         elif file.lower().endswith('.csv'):
-#             data = pd.read_csv(url)
-#             return data
-#         return False
-        
-#     file_df = load_data()
-#     df = pd.DataFrame(file_df)
-    
-#     # to json
-#     result = df.to_json(orient="records")
-#     parsed = json.loads(result)
-
-#     print(df)
-#     insert_to_redis(file, parsed)
-    
-#     # Akui bahwa kami menangani pesan tanpa masalah.
-#     message.ack()
+        message.ack()
